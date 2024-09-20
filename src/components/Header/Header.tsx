8b3eb01aabe4555d4f676667c61ec88bbd92699d
akbarsharofiddinov <akbarjonsharofiddino2004@gmail.com>
2024-09-20 21:04:34 +0500

    .

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import logo from "../../assets/logo.png";
+import logoBanner from "../../assets/logo-banner.png";
 
 const Header: React.FC = () => {
   return (
     <div className="container">
-      <header className="header">
+      <header
+        className="header"
+        style={{ backgroundImage: `url(${logoBanner})` }}
+      >
         <div className="logo">
           <a href="/" className="logo-link">
             <img src={logo} alt="logo image" />
